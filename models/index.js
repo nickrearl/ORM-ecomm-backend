db9140575cfc9fac97da1919e08085c4862d38b7
Nick Earl <nick.r.earl@gmail.com>
2022-08-18 20:05:17 -0400

    added cascading to Product/Category relationship

--- models/index.js
+++ models/index.js
@@ -6,12 +6,17 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
+  
 })
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE'
 })
 
 // Products belongToMany Tags (through ProductTag)
